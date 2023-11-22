2d8f46568c94c29c1d74c5db4abb175f6fdc64c8
yoon52 <143405479+yoon52@users.noreply.github.com>
2023-11-22 18:53:22 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -112,8 +112,8 @@ app.get('/user_info', (req, res) => {
 });
 
 
-// 사용자 정보 변경 엔드포인트
-app.post('/update', (req, res) => {
+// 사용자 이메일 변경 엔드포인트
+app.post('/change_email', (req, res) => {
   const updateData = req.body;
 
   // 유효성 검사
@@ -144,6 +144,41 @@ app.post('/update', (req, res) => {
     });
   });
 });
+
+
+// 사용자 비밀번호 변경 엔드포인트
+app.post('/change_password', (req, res) => {
+  const updateData = req.body;
+
+  // 유효성 검사
+  if (!updateData.id || !updateData.oldPassword || !updateData.newPassword) {
+    return res.status(400).json({ success: false, message: '누락된 데이터가 있습니다.' });
+  }
+
+  // 사용자 아이디와 기존 비밀번호로 사용자 정보를 데이터베이스에서 확인
+  db.query('SELECT * FROM users WHERE id = ? AND password = ?', [updateData.id, updateData.oldPassword], (error, results) => {
+    if (error) {
+      console.error(error);
+      return res.status(500).json({ success: false, message: '서버 오류' });
+    }
+
+    if (results.length === 0) {
+      // 일치하는 사용자가 없는 경우
+      return res.status(401).json({ success: false, message: '일치하는 사용자 정보가 없습니다.' });
+    }
+
+    // 사용자 비밀번호 변경
+    db.query('UPDATE users SET password = ? WHERE id = ?', [updateData.newPassword, updateData.id], (error, results) => {
+      if (error) {
+        console.error(error);
+        return res.status(500).json({ success: false, message: '서버 오류' });
+      }
+
+      res.status(200).json({ success: true, message: '비밀번호가 변경되었습니다.' });
+    });
+  });
+});
+
 
 
 // 게시글 작성 엔드포인트
@@ -184,21 +219,83 @@ app.get('/posts', (req, res) => {
   });
 });
 
-app.post('/saveToDatabase', (req, res) => {
-    const text = req.body.text;
 
-    // MySQL에 정보 삽입
-    const sql = `INSERT INTO texts (text) VALUES ('${text}')`;
-    db.query(sql, (err, result) => {
+// 사용자 탈퇴 엔드포인트
+app.post('/delete', (req, res) => {
+  const deleteData = req.body;
+
+  // 유효성 검사
+  if (!deleteData.id) {
+    return res.status(400).json({ success: false, message: '누락된 데이터가 있습니다.' });
+  }
+
+  // 사용자 아이디로 사용자 정보를 데이터베이스에서 확인
+  db.query('SELECT * FROM users WHERE id = ?', [deleteData.id], (error, results) => {
+    if (error) {
+      console.error(error);
+      return res.status(500).json({ success: false, message: '서버 오류' });
+    }
+
+    if (results.length === 0) {
+      // 일치하는 사용자가 없는 경우
+      return res.status(401).json({ success: false, message: '일치하는 사용자 정보가 없습니다.' });
+    }
+
+    // 사용자 정보 삭제
+    db.query('DELETE FROM users WHERE id = ?', [deleteData.id], (error, results) => {
+      if (error) {
+        console.error(error);
+        return res.status(500).json({ success: false, message: '서버 오류' });
+      }
+
+      res.status(200).json({ success: true, message: '사용자 정보가 삭제되었습니다.' });
+    });
+  });
+});
+
+
+// /books 엔드포인트에 대한 GET 요청 처리
+app.get('/books', (req, res) => {
+  const userId = req.query.userId; // 사용자 ID
+
+  // 사용자 ID에 해당하는 책 목록을 가져오기 위한 SQL 쿼리
+  const sql = 'SELECT * FROM texts WHERE user_id = ?'; // 쿼리 수정: userId -> user_id
+
+  // 데이터베이스에서 책 목록을 가져옵니다.
+  db.query(sql, [userId], (err, result) => {
     if (err) {
-      console.error('Error saving data to database:', err);
-      res.status(500).send('Error saving data to database');
+      console.error('Error fetching books from database:', err);
+      res.status(500).send('Error fetching books from database');
       return;
     }
-      console.log('Data saved to database');
-      res.status(200).send('Data saved to database');
-    });
+
+
+    // 책 목록을 JSON 형태로 클라이언트에게 반환합니다.
+    res.json(result);
+  });
 });
+
+
+app.post('/saveToDatabase', (req, res) => {
+  const data = req.body;
+      const userid = data.userid;
+      const texts = data.texts;
+  
+      const values = texts.map(({ title, author }) => `('${userid}', '${title}', '${author}')`).join(', ');
+    
+      // MySQL에 정보 삽입
+      const sql = `INSERT INTO texts (user_id, title, author) VALUES ${values}`;
+  
+      db.query(sql, (err, result) => {
+          if (err) {
+              console.error('Error saving data to database:', err);
+              res.status(500).send('Error saving data to database');
+              return;
+          }
+          console.log('Data saved to database');
+          res.status(200).send('Data saved to database');
+      });
+  });
 
 
 app.listen(port, () => {
